f544183664c9aadccc57331380dba5b742f4c2d7
Thomas Json <joogletom8@gmail.com>
2022-09-08 11:04:12 +0200

    Product-img

--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -3,11 +3,11 @@ import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import './product.scss';
 
-const Product = ({ title, price, description }) => {
+const Product = ({ title, price, description, image }) => {
   return (
     <div>
       <Card className="card-style">
-        <Card.Img variant="top" src="holder.js/100px180" />
+        <Card.Img variant="top" src={image} />
         <Card.Body>
           <Card.Title>{title}</Card.Title>
           <Card.Text>{description}</Card.Text>
